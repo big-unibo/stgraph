c386eada09f4334630334f1c5bd228601db0e525
w4bo <you@example.com>
2026-07-31 13:28:31 +0000

    ci: fix run script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -xo
 
 cp src/main/resources/test_config.example.yml src/main/resources/test_config.yml
 cp src/main/resources/mimic-iv_subjectids_tsids_short.csv src/main/resources/mimic-iv_subjectids_tsids.csv
-./downloadDataset.sh smartbench small.tar.gz
+./downloadDataset.sh smartbench xsmall.tar.gz
 ./downloadDataset.sh mimic 1692200.dump
 docker compose up -d
 sleep 150
