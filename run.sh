af1befe58ab171f7f54ca11ce7232bca7d4244b4
mfrancia <m.francia@unibo.it>
2026-07-31 15:14:31 +0200

    ci: working on ci

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#/bin/bash
-set -xo
+#!/bin/bash
+set -x
 
 cp src/main/resources/test_config.example.yml src/main/resources/test_config.yml
 cp src/main/resources/mimic-iv_subjectids_tsids_short.csv src/main/resources/mimic-iv_subjectids_tsids.csv
